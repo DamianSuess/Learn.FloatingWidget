4bdb9388f66d442bdfbce6c86420c3ae08826b04
Damian Suess <djsuess@gmail.com>
2021-12-31 13:27:34 -0500

    Added layout FloatingWidgetService

--- source/Learn.FloatingWidget.Android/MainApplication.cs
+++ source/Learn.FloatingWidget.Android/MainApplication.cs
@@ -1,23 +1,26 @@
 using System;
 using Android.App;
 using Android.Runtime;
+using Learn.FloatingWidget.Droid.Services;
 
 namespace Learn.FloatingWidget.Droid
 {
-    [Application(
-        Theme = "@style/MainTheme"
-        )]
-    public class MainApplication : Application
+  [Application(Theme = "@style/MainTheme")]
+  public class MainApplication : Application
+  {
+    public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+      : base(javaReference, transfer)
     {
-        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
-            : base(javaReference, transfer)
-        {
-        }
-
-        public override void OnCreate()
-        {
-            base.OnCreate();
-            Xamarin.Essentials.Platform.Init(this);
-        }
     }
+
+    public override void OnCreate()
+    {
+      base.OnCreate();
+      Xamarin.Essentials.Platform.Init(this);
+
+      SetContentView(Resource.Layout.Main);
+
+      StartService(new Android.Content.Intent(this, typeof(FloatingWidgetService)));
+    }
+  }
 }
---
+++ source/Learn.FloatingWidget.Android/Services/FloatingWidgetService.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+
+namespace Learn.FloatingWidget.Droid.Services
+{
+  [Service]
+  public class FloatingWidgetService : Service, View.IOnTouchListener
+  {
+    private IWindowManager _windowManager;
+    private WindowManagerLayoutParams _layoutParams;
+    private View _floatingView;
+
+    private int _initialX;
+    private int _initialY;
+    private float _initialTouchX;
+    private float _initialTouchY;
+
+    public override IBinder OnBind(Intent intent)
+    {
+      return null;
+    }
+
+    public override void OnCreate()
+    {
+      base.OnCreate();
+      _floatingView = LayoutInflater.From(this).Inflate(Resource.Layout.LayoutFloatingWidget, null);
+
+      _layoutParams = new WindowManagerLayoutParams(
+        ViewGroup.LayoutParams.WrapContent,
+        ViewGroup.LayoutParams.WrapContent,
+        WindowManagerTypes.Phone,
+        WindowManagerFlags.NotFocusable,
+        Format.Translucent)
+      {
+        Gravity = GravityFlags.Left | GravityFlags.Top
+      };
+
+      _windowManager = GetSystemService(WindowService).JavaCast<IWindowManager>();
+      _windowManager.AddView(_floatingView, _layoutParams);
+    }
+
+    public override void OnDestroy()
+    {
+      base.OnDestroy();
+
+      if (_floatingView != null)
+      {
+        _windowManager.RemoveView(_floatingView);
+      }
+    }
+
+    public bool OnTouch(View view, MotionEvent motionEvent)
+    {
+      bool result = false;
+
+      switch (motionEvent.Action)
+      {
+        case MotionEventActions.Down:
+
+          // Initial Position on press
+          _initialX = _layoutParams.X;
+          _initialY = _layoutParams.Y;
+
+          // Touch Position
+          _initialTouchX = motionEvent.RawX;
+          _initialTouchY = motionEvent.RawY;
+
+          result = true;
+          break;
+
+        case MotionEventActions.Move:
+
+          // Calculate the X & Y coords of the View
+          _layoutParams.X = _initialX + (int)(motionEvent.RawX - _initialTouchX);
+          _layoutParams.Y = _initialY + (int)(motionEvent.RawY - _initialTouchY);
+
+          // Move the view on the screen
+          _windowManager.UpdateViewLayout(_floatingView, _layoutParams);
+
+          result = true;
+          break;
+      }
+
+      return result;
+    }
+  }
+}
